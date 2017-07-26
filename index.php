188ab76f2a5ada50519f503f887a4d97dde7ccf6
SDWilked <simonwilkes@gmail.com>
2017-07-26 10:41:53 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+    <head>
+        <?php
+            include "DocumentHead.php";
+        ?>
+    </head>
+    <body>
+        <?php
+            include "header.php";
+            include "breadcrumbs.php";
+            include "content.php";
+            include "NewsletterSignup.php";
+            include "footer.php";
+        ?>
+    </body>
