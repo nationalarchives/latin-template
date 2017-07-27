7d9328338352a76b9860e1d257e5a707be214fde
SDWilked <simonwilkes@gmail.com>
2017-07-27 15:27:19 +0100

    many many changes - full revamp of landing page

---
+++ LandingPage.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+    <head>
+    <title>Learn Latin</title>
+        <?php
+            include "DocumentHead.php";
+            include "./constants/header.php";
+        ?>
+    </head>
+
+    <body>
+        <!-- breadcrumbs start -->
+
+        <div id="breadcrumb-holder" class="tna-breadcrumb">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="breadcrumbs"><a href="http://www.nationalarchives.gov.uk/">Home</a>
+                            <span class="sep">&gt;</span> <span><a href="http://www.nationalarchives.gov.uk/help-with-your-research/">Help with your research</a></span>
+                            <span class="sep">&gt;</span> <span><a href="http://www.nationalarchives.gov.uk/help-with-your-research/reading-old-documents/">Reading old documents</a></span>
+                            <span class="sep">&gt;</span> <span>Learn Latin 1086 - 1733</span>
+                        </div>
+                        <!-- .breadcrumbs -->
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- breadcrumbs end -->
+
+        <!-- main content start -->
+
+        <main id="primary" role="main" class="content-area">
+            <div class="container">
+                <!-- Banner -->
+                <div class="row level-one" role="banner">
+                    <div class="col-md-12">
+                        <article class="banner feature-img feature-img-bg" style="background-image: url(
+                        ./images/domesdayBG.jpg);">
+                            <div class="entry-header">
+                                <h1>Learn Latin 1086 - 1733</h1>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+                <!-- after banner para -->
+                <div class="row">
+                    <div class="col-md-12">
+                        <article>
+                            <div class="entry-content clearfix">
+                                <h2>A practical online tutorial for beginners.</h2>
+                                <p>This tutorial is a handy guide to Latin as used in England between 1086 and 1733, when it was the general language for official documents (after 1733 English became the widely-used language for these documents); Knowing Latin will help you to read documents from this period.
+                                </p>
+                                <p>No previous knowledge of Latin is required - this tutorial is suitable for anyone who wants to learn.</p>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+                <!-- Tutorials -->
+                <div class="row">
+                    <!-- Beginners' -->
+                    <div class="col-md-6 col-xs-12">
+                        <article>
+                            <div class="entry-header">
+                                <h2>
+                                    <a href="">Beginners' Tutorials</a>
+                                </h2>
+                            </div>
+                            <div class="entry-content clearfix">
+                                <h3><a href="">Lesson 1</a></h3>
+                                <ul>
+                                    <li><a href="" >Learn</a></li>
+                                    <li><a href="">Test Your Knowledge</a></li>
+                                </ul>
+                                <h3><a href="">Lesson 2</a></h3>
+                                <ul>
+                                    <li><a href="" >Learn</a></li>
+                                    <li><a href="">Test Your Knowledge</a></li>
+                                </ul>
+                                <h3><a href="">Lesson 3</a></h3>
+                                <ul>
+                                    <li><a href="" >Learn</a></li>
+                                    <li><a href="">Test Your Knowledge</a></li>
+                                </ul>
+                                <h3><a href="">Lesson 4</a></h3>
+                                <ul>
+                                    <li><a href="" >Learn</a></li>
+                                    <li><a href="">Test Your Knowledge</a></li>
+                                </ul>
+                                <h3><a href="">Lesson 5</a></h3>
+                                <ul>
+                                    <li><a href="" >Learn</a></li>
+                                    <li><a href="">Test Your Knowledge</a></li>
+                                </ul>
+                                <h3><a href="">Lesson 6</a></h3>
+                                <ul>
+                                    <li><a href="" >Learn</a></li>
+                                    <li><a href="">Test Your Knowledge</a></li>
+                                </ul>
+                                <h3><a href="">Lesson 7</a></h3>
+                                <ul>
+                                    <li><a href="" >Learn</a></li>
+                                    <li><a href="">Test Your Knowledge</a></li>
+                                </ul>
+                                <h3><a href="">Lesson 8</a></h3>
+                                <ul>
+                                    <li><a href="" >Learn</a></li>
+                                    <li><a href="">Test Your Knowledge</a></li>
+                                </ul>
+                                <h3><a href="">Lesson 9</a></h3>
+                                <ul>
+                                    <li><a href="" >Learn</a></li>
+                                    <li><a href="">Test Your Knowledge</a></li>
+                                </ul>
+                                <h3><a href="">Lesson 10</a></h3>
+                                <ul>
+                                    <li><a href="" >Learn</a></li>
+                                    <li><a href="">Test Your Knowledge</a></li>
+                                </ul>
+                                <h3><a href="">Lesson 11</a></h3>
+                                <ul>
+                                    <li><a href="" >Learn</a></li>
+                                    <li><a href="">Test Your Knowledge</a></li>
+                                </ul>
+                                <h3><a href="">Lesson 12</a></h3>
+                                <ul>
+                                    <li><a href="" >Learn</a></li>
+                                    <li><a href="">Test Your Knowledge</a></li>
+                                </ul>
+                            </div>
+                        </article>
+                    </div>
+                    <!-- Advanced -->
+                    <div class="col-md-6 col-xs-12">
+                        <article>
+                            <div class="entry-header">
+                                <h2>
+                                    <a href="">Advanced Tutorials</a>
+                                </h2>
+                            </div>
+                            <div class="entry-content clearfix">
+                                <h3><a href="">Lesson 1</a></h3>
+                                <ul>
+                                    <li><a href="" >Learn</a></li>
+                                    <li><a href="">Test Your Knowledge</a></li>
+                                </ul>
+                                <h3><a href="">Lesson 2</a></h3>
+                                <ul>
+                                    <li><a href="" >Learn</a></li>
+                                    <li><a href="">Test Your Knowledge</a></li>
+                                </ul>
+                                <h3><a href="">Lesson 3</a></h3>
+                                <ul>
+                                    <li><a href="" >Learn</a></li>
+                                    <li><a href="">Test Your Knowledge</a></li>
+                                </ul>
+                                <h3><a href="">Lesson 4</a></h3>
+                                <ul>
+                                    <li><a href="" >Learn</a></li>
+                                    <li><a href="">Test Your Knowledge</a></li>
+                                </ul>
+                                <h3><a href="">Lesson 5</a></h3>
+                                <ul>
+                                    <li><a href="" >Learn</a></li>
+                                    <li><a href="">Test Your Knowledge</a></li>
+                                </ul>
+                                <h3><a href="">Lesson 6</a></h3>
+                                <ul>
+                                    <li><a href="" >Learn</a></li>
+                                    <li><a href="">Test Your Knowledge</a></li>
+                                </ul>
+                                <h3><a href="">Lesson 7</a></h3>
+                                <ul>
+                                    <li><a href="" >Learn</a></li>
+                                    <li><a href="">Test Your Knowledge</a></li>
+                                </ul>
+                                <h3><a href="">Lesson 8</a></h3>
+                                <ul>
+                                    <li><a href="" >Learn</a></li>
+                                    <li><a href="">Test Your Knowledge</a></li>
+                                </ul>
+                                <h3><a href="">Lesson 9</a></h3>
+                                <ul>
+                                    <li><a href="" >Learn</a></li>
+                                    <li><a href="">Test Your Knowledge</a></li>
+                                </ul>
+                                <h3><a href="">Lesson 10</a></h3>
+                                <ul>
+                                    <li><a href="" >Learn</a></li>
+                                    <li><a href="">Test Your Knowledge</a></li>
+                                </ul>
+                                <h3><a href="">Lesson 11</a></h3>
+                                <ul>
+                                    <li><a href="" >Learn</a></li>
+                                    <li><a href="">Test Your Knowledge</a></li>
+                                </ul>
+                                <h3><a href="">Lesson 12</a></h3>
+                                <ul>
+                                    <li><a href="" >Learn</a></li>
+                                    <li><a href="">Test Your Knowledge</a></li>
+                                </ul>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <article>
+                            <div class="entry-header">
+                                <h2>
+                                    Other Resources:
+                                </h2>
+                            </div>
+                            <div class="entry-content clearfix">
+                                <ul>
+                                    <li><a href="">Glossary</a></li>
+                                    <li><a href="">Word List</a></li>
+                                    <li><a href="">Grammar Table</a></li>
+                                    <li><a href="">Dating Latin Documents</a></li>
+                                    <li><a href="">How to Decline Personal Names</a></li>
+                                    <li><a href="">Problems with Laitn and the Documents</a></li>
+                                </ul>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+
+            </div>
+        </main>
+        <!-- main content end -->
+        <?php
+
+                include "./constants/NewsletterSignup.php";
+
+        ?>
+    </body>
+
+    <footer id="footer" class="breather-top-bottom" role="contentinfo">
+        <?php
+        include "./constants/footer.php";
+        ?>
+
+    </footer>
