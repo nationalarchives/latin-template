e42be22285b8e12daa97af66acc3667f7bf9dc4e
SDWilked <simonwilkes@gmail.com>
2017-08-01 11:32:58 +0100

    Ash fixes the tab CSS

--- content-switch.js
+++ content-switch.js
@@ -4,13 +4,20 @@ var lessonNumberOfSections = 5;
 function HideAll() {
     for (var i = 0; i <= lessonNumberOfSections; i++) {
         var contentID = '#content-'+i;
+
         $(contentID).removeClass("show");
         $(contentID).addClass("hide");
     };
+
+    $("#lesson-content-buttons button").each(function(){
+
+        $("button").removeClass("btn-active");
+    });
 }
 
 $(HideAll());
 $('#content-1').addClass("show")
+$('button:first-of-type').addClass("btn-active");
 
 $('#lesson-content-buttons button').on('click', function() {
 
@@ -18,6 +25,10 @@ $('#lesson-content-buttons button').on('click', function() {
 
 	var $this = $(this),
 		target_id = ('#' + $this.data('target'));
+
+
+    $this.addClass("btn-active");
+
 
     $(target_id).removeClass("hide");
 	$(target_id).addClass("show");
