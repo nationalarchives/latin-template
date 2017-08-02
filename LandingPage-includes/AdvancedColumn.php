6ae240c39b51bddd821a863d9d935a66139b8268
Annabel <annabel.baynes@gmail.com>
2017-08-02 09:32:29 +0100

    I've now put the content of the Landing Page sections into seperate includes

--- LandingPage-includes/AdvancedColumn.php
+++ LandingPage-includes/AdvancedColumn.php
@@ -1,7 +1,71 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: annabel
- * Date: 02/08/17
- * Time: 09:26
- */
+<div class="col-md-6 col-xs-12">
+    <article>
+        <div class="entry-header">
+            <h2>
+                <a href="">Advanced Tutorials</a>
+            </h2>
+        </div>
+        <div class="entry-content clearfix">
+            <h3><a href="">Lesson 1</a></h3>
+            <ul>
+                <li><a href="" >Learn</a></li>
+                <li><a href="">Test Your Knowledge</a></li>
+            </ul>
+            <h3><a href="">Lesson 2</a></h3>
+            <ul>
+                <li><a href="" >Learn</a></li>
+                <li><a href="">Test Your Knowledge</a></li>
+            </ul>
+            <h3><a href="">Lesson 3</a></h3>
+            <ul>
+                <li><a href="" >Learn</a></li>
+                <li><a href="">Test Your Knowledge</a></li>
+            </ul>
+            <h3><a href="">Lesson 4</a></h3>
+            <ul>
+                <li><a href="" >Learn</a></li>
+                <li><a href="">Test Your Knowledge</a></li>
+            </ul>
+            <h3><a href="">Lesson 5</a></h3>
+            <ul>
+                <li><a href="" >Learn</a></li>
+                <li><a href="">Test Your Knowledge</a></li>
+            </ul>
+            <h3><a href="">Lesson 6</a></h3>
+            <ul>
+                <li><a href="" >Learn</a></li>
+                <li><a href="">Test Your Knowledge</a></li>
+            </ul>
+            <h3><a href="">Lesson 7</a></h3>
+            <ul>
+                <li><a href="" >Learn</a></li>
+                <li><a href="">Test Your Knowledge</a></li>
+            </ul>
+            <h3><a href="">Lesson 8</a></h3>
+            <ul>
+                <li><a href="" >Learn</a></li>
+                <li><a href="">Test Your Knowledge</a></li>
+            </ul>
+            <h3><a href="">Lesson 9</a></h3>
+            <ul>
+                <li><a href="" >Learn</a></li>
+                <li><a href="">Test Your Knowledge</a></li>
+            </ul>
+            <h3><a href="">Lesson 10</a></h3>
+            <ul>
+                <li><a href="" >Learn</a></li>
+                <li><a href="">Test Your Knowledge</a></li>
+            </ul>
+            <h3><a href="">Lesson 11</a></h3>
+            <ul>
+                <li><a href="" >Learn</a></li>
+                <li><a href="">Test Your Knowledge</a></li>
+            </ul>
+            <h3><a href="">Lesson 12</a></h3>
+            <ul>
+                <li><a href="" >Learn</a></li>
+                <li><a href="">Test Your Knowledge</a></li>
+            </ul>
+        </div>
+    </article>
+</div>
