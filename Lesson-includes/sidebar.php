adf11e0984417553e0b884ce02d3e9a1e7a256f5
Annabel <annabel.baynes@gmail.com>
2017-08-02 14:57:18 +0100

    The sidebar now contains correct links to both beginners and advanced latin pages

--- Lesson-includes/sidebar.php
+++ Lesson-includes/sidebar.php
@@ -7,82 +7,227 @@
                     <a href="./LandingPage.php">Back to Latin 1086 - 1733</a>
                 </h2>
             </div>
-			<!-- LESSON 1 LINKS -->
-			<div class="clearfix">
-				<h3><a href="./LessonTemplate.php?Lesson=<?php echo 1 ?>">
-					Lesson 1
-				</a></h3>
-			</div>
-			<!-- LESSON 2 LINKS -->
-			<div class="clearfix">
-				<h3><a href="./LessonTemplate.php?Lesson=<?php echo 2 ?>">
-					Lesson 2
-				</a></h3>
-			</div>
-			<!-- LESSON 3 LINKS -->
-			<div class="clearfix">
-				<h3><a href="./LessonTemplate.php?Lesson=<?php echo 3 ?>">
-					Lesson 3
-				</a></h3>
-			</div>
-			<!-- LESSON 4 LINKS -->
-			<div class="clearfix">
-				<h3><a href="./LessonTemplate.php?Lesson=<?php echo 4 ?>">
-					Lesson 4
-				</a></h3>
-			</div>
-			<!-- LESSON 5 LINKS -->
-			<div class="clearfix">
-				<h3><a href="./LessonTemplate.php?Lesson=<?php echo 5 ?>">
-					Lesson 5
-				</a></h3>
-			</div>
-			<!-- LESSON 6 LINKS -->
-			<div class="clearfix">
-				<h3><a href="./LessonTemplate.php?Lesson=<?php echo 6 ?>">
-					Lesson 6
-				</a></h3>
-			</div>
-			<!-- LESSON 7 LINKS -->
-			<div class="clearfix">
-				<h3><a href="./LessonTemplate.php?Lesson=<?php echo 7 ?>">
-					Lesson 7
-				</a></h3>
-			</div>
-			<!-- LESSON 8 LINKS -->
-			<div class="clearfix">
-				<h3><a href="./LessonTemplate.php?Lesson=<?php echo 8 ?>">
-					Lesson 8
-				</a></h3>
-			</div>
-			<!-- LESSON 9 LINKS -->
-			<div class="clearfix">
-				<h3><a href="./LessonTemplate.php?Lesson=<?php echo 9 ?>">
-					Lesson 9
-				</a></h3>
-			</div>
-			<!-- LESSON 10 LINKS -->
-			<div class="clearfix">
-				<h3><a href="./LessonTemplate.php?Lesson=<?php echo 10 ?>">
-					Lesson 10
-				</a></h3>
-			</div>
-			<!-- LESSON 11 LINKS -->
-			<div class="clearfix">
-				<h3><a href="./LessonTemplate.php?Lesson=<?php echo 11 ?>">
-					Lesson 11
-				</a></h3>
-			</div>
-			<!-- LESSON 12 LINKS -->
-			<div class="clearfix">
-				<h3><a href="./LessonTemplate.php?Lesson=<?php echo 12 ?>">
-					Lesson 12
-				</a></h3>
-			</div>
-			<div class="clearfix"></div>
+            <div class="entry-content clearfix">
+                <h2>Beginners' Latin</h2>
+                <h3>
+                    <a href="./LessonTemplate.php?Difficulty=<?php echo "beginners" ?>&amp;Lesson=<?php echo 1 ?>">
+                        Lesson 1
+                    </a>
+                </h3>
+                <ul>
+                    <li><a href="" >Learn</a></li>
+                    <li><a href="">Test Your Knowledge</a></li>
+                </ul>
+                <h3>
+                    <a href="./LessonTemplate.php?Difficulty=<?php echo "beginners" ?>&amp;Lesson=<?php echo 2 ?>">
+                        Lesson 2
+                    </a>
+                </h3>
+                <ul>
+                    <li><a href="" >Learn</a></li>
+                    <li><a href="">Test Your Knowledge</a></li>
+                </ul>
+                <h3>
+                    <a href="./LessonTemplate.php?Difficulty=<?php echo "beginners" ?>&amp;Lesson=<?php echo 3 ?>">
+                        Lesson 3
+                    </a>
+                </h3>
+                <ul>
+                    <li><a href="" >Learn</a></li>
+                    <li><a href="">Test Your Knowledge</a></li>
+                </ul>
+                <h3>
+                    <a href="./LessonTemplate.php?Difficulty=<?php echo "beginners" ?>&amp;Lesson=<?php echo 4 ?>">
+                        Lesson 4
+                    </a>
+                </h3>
+                <ul>
+                    <li><a href="" >Learn</a></li>
+                    <li><a href="">Test Your Knowledge</a></li>
+                </ul>
+                <h3>
+                    <a href="./LessonTemplate.php?Difficulty=<?php echo "beginners" ?>&amp;Lesson=<?php echo 5 ?>">
+                        Lesson 5
+                    </a>
+                </h3>
+                <ul>
+                    <li><a href="" >Learn</a></li>
+                    <li><a href="">Test Your Knowledge</a></li>
+                </ul>
+                <h3>
+                    <a href="./LessonTemplate.php?Difficulty=<?php echo "beginners" ?>&amp;Lesson=<?php echo 6 ?>">
+                        Lesson 6
+                    </a>
+                </h3>
+                <ul>
+                    <li><a href="" >Learn</a></li>
+                    <li><a href="">Test Your Knowledge</a></li>
+                </ul>
+                <h3>
+                    <a href="./LessonTemplate.php?Difficulty=<?php echo "beginners" ?>&amp;Lesson=<?php echo 7 ?>">
+                        Lesson 7
+                    </a>
+                </h3>
+                <ul>
+                    <li><a href="" >Learn</a></li>
+                    <li><a href="">Test Your Knowledge</a></li>
+                </ul>
+                <h3>
+                    <a href="./LessonTemplate.php?Difficulty=<?php echo "beginners" ?>&amp;Lesson=<?php echo 8 ?>">
+                        Lesson 8
+                    </a>
+                </h3>
+                <ul>
+                    <li><a href="" >Learn</a></li>
+                    <li><a href="">Test Your Knowledge</a></li>
+                </ul>
+                <h3>
+                    <a href="./LessonTemplate.php?Difficulty=<?php echo "beginners" ?>&amp;Lesson=<?php echo 9 ?>">
+                        Lesson 9
+                    </a>
+                </h3>
+                <ul>
+                    <li><a href="" >Learn</a></li>
+                    <li><a href="">Test Your Knowledge</a></li>
+                </ul>
+                <h3>
+                    <a href="./LessonTemplate.php?Difficulty=<?php echo "beginners" ?>&amp;Lesson=<?php echo 10 ?>">
+                        Lesson 10
+                    </a>
+                </h3>
+                <ul>
+                    <li><a href="" >Learn</a></li>
+                    <li><a href="">Test Your Knowledge</a></li>
+                </ul>
+                <h3>
+                    <a href="./LessonTemplate.php?Difficulty=<?php echo "beginners" ?>&amp;Lesson=<?php echo 11 ?>">
+                        Lesson 11
+                    </a>
+                </h3>
+                <ul>
+                    <li><a href="" >Learn</a></li>
+                    <li><a href="">Test Your Knowledge</a></li>
+                </ul>
+                <h3>
+                    <a href="./LessonTemplate.php?Difficulty=<?php echo "beginners" ?>&amp;Lesson=<?php echo 12 ?>">
+                        Lesson 12
+                    </a>
+                </h3>
+                <ul>
+                    <li><a href="" >Learn</a></li>
+                    <li><a href="">Test Your Knowledge</a></li>
+                </ul>
+                <h2>Advanced Latin</h2>
+                <h3>
+                    <a href="./LessonTemplate.php?Difficulty=<?php echo "advanced" ?>&amp;Lesson=<?php echo 1 ?>">
+                        1. Imperfect Tense
+                    </a>
+                </h3>
+                <ul>
+                    <li><a href="" >Learn</a></li>
+                    <li><a href="">Test Your Knowledge</a></li>
+                </ul>
+                <h3>
+                    <a href="./LessonTemplate.php?Difficulty=<?php echo "advanced" ?>&amp;Lesson=<?php echo 2 ?>">
+                        2. Pluperfect Tense
+                    </a>
+                </h3>
+                <ul>
+                    <li><a href="" >Learn</a></li>
+                    <li><a href="">Test Your Knowledge</a></li>
+                </ul>
+                <h3>
+                    <a href="./LessonTemplate.php?Difficulty=<?php echo "advanced" ?>&amp;Lesson=<?php echo 3 ?>">
+                        3. Future Simple Tense
+                    </a>
+                </h3>
+                <ul>
+                    <li><a href="" >Learn</a></li>
+                    <li><a href="">Test Your Knowledge</a></li>
+                </ul>
+                <h3>
+                    <a href="./LessonTemplate.php?Difficulty=<?php echo "advanced" ?>&amp;Lesson=<?php echo 4 ?>">
+                        4. Future Perfect Tense
+                    </a>
+                </h3>
+                <ul>
+                    <li><a href="" >Learn</a></li>
+                    <li><a href="">Test Your Knowledge</a></li>
+                </ul>
+                <h3>
+                    <a href="./LessonTemplate.php?Difficulty=<?php echo "advanced" ?>&amp;Lesson=<?php echo 5 ?>">
+                        5. Pronouns
+                    </a>
+                </h3>
+                <ul>
+                    <li><a href="" >Learn</a></li>
+                    <li><a href="">Test Your Knowledge</a></li>
+                </ul>
+                <h3>
+                    <a href="./LessonTemplate.php?Difficulty=<?php echo "advanced" ?>&amp;Lesson=<?php echo 6 ?>">
+                        6. Passive Verbs
+                    </a>
+                </h3>
+                <ul>
+                    <li><a href="" >Learn</a></li>
+                    <li><a href="">Test Your Knowledge</a></li>
+                </ul>
+                <h3>
+                    <a href="./LessonTemplate.php?Difficulty=<?php echo "advanced" ?>&amp;Lesson=<?php echo 7 ?>">
+                        7. Participles - Present, Past and Future
+                    </a>
+                </h3>
+                <ul>
+                    <li><a href="" >Learn</a></li>
+                    <li><a href="">Test Your Knowledge</a></li>
+                </ul>
+                <h3>
+                    <a href="./LessonTemplate.php?Difficulty=<?php echo "advanced" ?>&amp;Lesson=<?php echo 8 ?>">
+                        8. Comparison of Adjectives and Adverbs
+                    </a>
+                </h3>
+                <ul>
+                    <li><a href="" >Learn</a></li>
+                    <li><a href="">Test Your Knowledge</a></li>
+                </ul>
+                <h3>
+                    <a href="./LessonTemplate.php?Difficulty=<?php echo "advanced" ?>&amp;Lesson=<?php echo 9 ?>">
+                        9. Subjunctive
+                    </a>
+                </h3>
+                <ul>
+                    <li><a href="" >Learn</a></li>
+                    <li><a href="">Test Your Knowledge</a></li>
+                </ul>
+                <h3>
+                    <a href="./LessonTemplate.php?Difficulty=<?php echo "advanced" ?>&amp;Lesson=<?php echo 10 ?>">
+                        10. Deponent and Semi-Deponent Verbs
+                    </a>
+                </h3>
+                <ul>
+                    <li><a href="" >Learn</a></li>
+                    <li><a href="">Test Your Knowledge</a></li>
+                </ul>
+                <h3>
+                    <a href="./LessonTemplate.php?Difficulty=<?php echo "advanced" ?>&amp;Lesson=<?php echo 11 ?>">
+                        11. Gerunds and Gerundives
+                    </a>
+                </h3>
+                <ul>
+                    <li><a href="" >Learn</a></li>
+                    <li><a href="">Test Your Knowledge</a></li>
+                </ul>
+                <h3>
+                    <a href="./LessonTemplate.php?Difficulty=<?php echo "advanced" ?>&amp;Lesson=<?php echo 12 ?>">
+                        12. Infinitives; Accusative and Infinitive Clause
+                    </a>
+                </h3>
+                <ul>
+                    <li><a href="" >Learn</a></li>
+                    <li><a href="">Test Your Knowledge</a></li>
+                </ul>
+            </div>
 		</div>
-		<div class="clearfix"></div>
 	</div>
-	<div class="clearfix"></div>
 </article>
 </aside>
