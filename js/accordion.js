a8b939c00dfd8ec1b2a748b0f1568099c320a66a
Annabel <annabel.baynes@gmail.com>
2017-08-03 10:59:54 +0100

    Have now added accordion functionality!
    buttons are still ugly as heck though

--- js/accordion.js
+++ js/accordion.js
@@ -1,26 +1,31 @@
-function HideAll() {
-    $("div.accordion-panel").each(function (){
-        var $this = $(this);
-        if ($this.hasClass("show")) {
-            $this.removeClass("show");
-            $this.addClass("hide");
-        }
-    });
+function HideAll(isFullHide) {
+
+    if (isFullHide) {
+        $("div.accordion-panel").each(function (){
+            $(this).removeClass("show");
+            $(this).addClass("hide");
+        });
+    } else {
+        $("div.accordion-panel:not(#currentpanel)").each(function() {
+            $(this).removeClass("show");
+            $(this).addClass("hide");
+        });
+    }
+
+    $("div.accordion-panel#currentpanel").removeAttr("id");
+
 }
 
-$(HideAll());
+$(HideAll(true));
 
 $("button.accordion").on('click', function() {
     var $this = $(this),
         nextPanel = $this.next("div.accordion-panel");
-    console.log("HEP HELP IM BEING REPRESSED")
-    $this.toggleClass("btn-active");
+    $(nextPanel).attr("id", "currentpanel") ;
 
-    if ($(nextPanel).hasClass("show")) {
-        $(nextPanel).removeClass("show");
-        $(nextPanel).addClass("hide");
-    } else {
-        $(nextPanel).addClass("show");
-        $(nextPanel).removeClass("hide");
-    }
+    HideAll(false);
+
+    $this.toggleClass("btn-active");
+    $(nextPanel).toggleClass("show");
+    $(nextPanel).toggleClass("hide");
 });
